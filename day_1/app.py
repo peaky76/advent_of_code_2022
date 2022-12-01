a2a8ae626e8473b9172d04df386b34a3e71a5085
Robert Peacock <robertjamespeacock@gmail.com>
2022-12-01 22:38:19 +0000

    Day 1 complete

---
+++ day_1/app.py
@@ -0,0 +1,25 @@
+from input import Input
+import heapq
+
+def get_elf_max_calorie_counts(data, n = 1):
+    elf_totals = []
+    current_calorie_count = 0
+    for food_item in data:
+        if food_item:
+            current_calorie_count += food_item
+        else:
+            elf_totals.append(current_calorie_count)
+            current_calorie_count = 0
+    elf_totals.append(current_calorie_count)
+
+    return sum(heapq.nlargest(n, elf_totals))
+
+input = Input(int)
+# PART ONE
+# print(get_elf_max_calorie_counts(input.example))
+print(get_elf_max_calorie_counts(input.puzzle)) 
+
+# PART TWO
+# print(get_elf_max_calorie_counts(input.example, 3))
+print(get_elf_max_calorie_counts(input.puzzle, 3)) 
+
---
+++ day_1/input.py
@@ -0,0 +1,15 @@
+class Input:
+
+    def __init__(self, cast):
+        self.cast = cast
+
+    @property
+    def example(self):
+        return self._read_input("example_input.txt")
+
+    @property
+    def puzzle(self):
+        return self._read_input("puzzle_input.txt")
+
+    def _read_input(self, file):
+        return [self.cast(line.rstrip('\n')) if line.rstrip('\n') else None for line in open(file, "r")]

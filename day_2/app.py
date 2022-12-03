c450e7e8c8c33fdcabd51d1e2a1c31190a27d908
Robert Peacock <robertjamespeacock@gmail.com>
2022-12-03 11:53:01 +0000

    Day 2 refactor

--- day_2/app.py
+++ day_2/app.py
@@ -3,56 +3,60 @@ import sys
 sys.path.append("..")
 from input import Input
 
-SHAPES = {
+CHOICES = {
     'A': 'ROCK',
     'B': 'PAPER',
     'C': 'SCISSORS',
-    'X': 'ROCK',
-    'Y': 'PAPER',
-    'Z': 'SCISSORS'
 }
 
-RESULTS = {
-    'X': 0,
-    'Y': 3,
-    'Z': 6
+SCORES = {
+    'ROCK': 1, 
+    'PAPER': 2, 
+    'SCISSORS': 3
 }
 
-WINS = [
-    ['SCISSORS', 'PAPER'],
-    ['PAPER', 'ROCK'],
-    ['ROCK', 'SCISSORS']
-] 
+WINS = {
+    'SCISSORS': 'PAPER',
+    'PAPER': 'ROCK',
+    'ROCK': 'SCISSORS'
+}
+
+CHOICE_STRATEGY = {
+    'X': lambda them: 'ROCK',
+    'Y': lambda them: 'PAPER',
+    'Z': lambda them: 'SCISSORS'
+}
+
+RESULT_STRATEGY = {
+    'X': lambda them: WINS[them], 
+    'Y': lambda them: them, 
+    'Z': lambda them: {v: k for k, v in WINS.items()}[them]
+}
+
+def determine_choices(match, strategy):
+    them_letter, you_letter = match.split(' ')
+    them = CHOICES[them_letter]
+    you = strategy[you_letter](them)
+    return (them, you)
 
 def game_score(them, you):
-    return 6 if [you, them] in WINS else 3 if them == you else 0
+    return 6 if WINS[you] == them else 3 if them == you else 0
 
 def choice_score(you):
-    return {'ROCK': 1, 'PAPER': 2, 'SCISSORS': 3}[you]
+    return SCORES[you]
 
-def total(matches, xyz_interpretation):
+def total(matches, strategy):
     total = 0
     for match in matches:
-        if xyz_interpretation == SHAPES:
-            them, you = (SHAPES[x] for x in match.split(' '))
-        else:
-            them = SHAPES[match.split(' ')[0]]
-            you_result = RESULTS[match.split(' ')[1]]
-            if you_result == 6:
-                you = [w[0] for w in WINS if w[1] == them][0]
-            elif you_result == 3:
-                you = them
-            else:
-                you = [w[1] for w in WINS if w[0] == them][0]    
-
+        them, you = determine_choices(match, strategy)        
         total += game_score(them, you) + choice_score(you)
     return total
     
 input = Input()
 # PART ONE
-# print(total(input.example, SHAPES))
-print(total(input.puzzle, SHAPES)) 
+# print(total(input.example, CHOICE_STRATEGY))
+print(total(input.puzzle, CHOICE_STRATEGY)) 
 
 # PART TWO
-# print(total(input.example, RESULTS))
-print(total(input.puzzle, RESULTS)) 
+# print(total(input.example, RESULT_STRATEGY))
+print(total(input.puzzle, RESULT_STRATEGY)) 

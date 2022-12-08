4c9acf00c9894b8c2ca746e5758d57a494b5094f
Robert Peacock <robertjamespeacock@gmail.com>
2022-12-08 13:35:02 +0000

    Day 8 Part 1 refactor

--- day_8/app.py
+++ day_8/app.py
@@ -5,23 +5,17 @@ from input import Input
 
 def is_visible(matrix, x, y):
     sightlines = [
-        [matrix[z][y] for z in range(0, x)],
+        [matrix[z][y] for z in range(x)],
         [matrix[z][y] for z in range(x + 1, len(matrix))],
-        [matrix[x][z] for z in range(0, y)],
+        [matrix[x][z] for z in range(y)],
         [matrix[x][z] for z in range(y + 1, len(matrix[0]))]
     ]
     target = matrix[x][y]
     return any(all(tree < target for tree in sightline) for sightline in sightlines)
 
-def exterior_trees_count(matrix):
-    return (2 * len(matrix)) + (2 * (len(matrix[0]) - 2))
-
-def interior_trees_count(matrix):
-    return sum(is_visible(matrix, row, col) for row in range(1, len(matrix) - 1) for col in range(1, len(matrix[0]) - 1))
-
 def count_visible_trees(source):
     matrix = [[int(tree) for tree in row] for row in source]
-    return exterior_trees_count(matrix) + interior_trees_count(matrix)
+    return sum(is_visible(matrix, row, col) for row in range(len(matrix)) for col in range(len(matrix[0])))
 
 input = Input()
 

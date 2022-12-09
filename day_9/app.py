4b07e4144698ee262469c122bdc68447cff246e1
Robert Peacock <robertjamespeacock@gmail.com>
2022-12-09 16:58:03 +0000

    Day 9 refactor

--- day_9/app.py
+++ day_9/app.py
@@ -14,21 +14,22 @@ def change_t_loc(t_loc, h_loc):
     hx, hy = h_loc
     x_diff = abs(tx - hx)
     y_diff = abs(ty - hy)
+    x_adjacent = hx - 1 if hx > tx else hx + 1
+    y_adjacent = hy - 1 if hy > ty else hy + 1
+    
     if x_diff <= 1 and y_diff <= 1:
         pass
-    elif ty == hy:
-        tx = hx - 1 if hx > tx else hx + 1
-    elif tx == hx:
-        ty = hy - 1 if hy > ty else hy + 1
-    elif x_diff < y_diff:
-        tx = hx
-        ty = hy - 1 if hy > ty else hy + 1
-    elif x_diff > y_diff:
-        tx = hx - 1 if hx > tx else hx + 1
-        ty = hy
-    else:
-        tx = hx - 1 if hx > tx else hx + 1
-        ty = hy - 1 if hy > ty else hy + 1
+    else:    
+        if x_diff >= y_diff or ty == hy:
+            tx = x_adjacent
+        elif ty != hy:
+            tx = hx
+
+        if x_diff <= y_diff or tx == hx:
+            ty = y_adjacent
+        elif tx != hx:
+            ty = hy
+            
     return (tx, ty)
 
 def trace_path(instruction_list, chain_links_n, start):
@@ -42,7 +43,6 @@ def trace_path(instruction_list, chain_links_n, start):
                 locs[0] = (locs[0][0], locs[0][1] + step)    
             for i, loc in enumerate(locs[1:]):
                 locs[i + 1] = change_t_loc(loc, locs[i])
-            # print(locs)
             yield locs[-1]   
 
 def count_distinct_locs(source, chain_links_n = 1, start = (0,0)):
@@ -50,20 +50,11 @@ def count_distinct_locs(source, chain_links_n = 1, start = (0,0)):
 
 input = Input()
 
-# TESTS
-# assert(change_t_loc((1, 1), (1, 1)) == (1, 1))
-# assert(change_t_loc((1, 1), (2, 1)) == (1, 1))
-# assert(change_t_loc((1, 1), (1, 2)) == (1, 1))
-# assert(change_t_loc((1, 1), (3, 1)) == (2, 1))
-# assert(change_t_loc((1, 4), (1, 2)) == (1, 3))
-# assert(change_t_loc((1, 1), (2, 3)) == (2, 2))
-# assert(change_t_loc((1, 1), (3, 2)) == (2, 2))
-
 # PART ONE
-print(count_distinct_locs(input.example))
+# print(count_distinct_locs(input.example))
 print(count_distinct_locs(input.puzzle))
 
 # PART TWO
-print(count_distinct_locs(input.example, 9))
-print(count_distinct_locs(input.bonus, 9))
+# print(count_distinct_locs(input.example, 9))
+# print(count_distinct_locs(input.bonus, 9))
 print(count_distinct_locs(input.puzzle, 9))

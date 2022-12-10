45a2f488ba08a10a719038a49295eb03fabccfa6
Robert Peacock <robertjamespeacock@gmail.com>
2022-12-10 13:02:12 +0000

    Day 10 complete

---
+++ day_10/app.py
@@ -0,0 +1,48 @@
+import sys
+# from collections import namedtuple
+
+sys.path.append("..")
+from input import Input
+
+def parse_line(line):
+    cycle_add = 1 if line == 'noop' else 2
+    after_effect = int(line.split(' ')[1]) if ' ' in line else 0
+    return (cycle_add, after_effect) 
+
+def x_during(n, source):
+    cycles = 0
+    x = 1
+    for line in [parse_line(line) for line in source]:
+        cycles += line[0]
+        if cycles >= n:
+            break
+        else:
+            x += line[1]
+    return x
+
+def signal_strength_during(n, source):
+    return n * x_during(n, source)
+
+def draw(source):
+    output = []
+    for i in range(0, 240):
+        sprite_centre = x_during(i + 1, source)
+        if abs((i % 40) - sprite_centre) <= 1:
+            output.append('#')
+        else:
+            output.append('.')
+        if (i + 1) % 40 == 0 and i != 0:
+            output.append('\n')
+    print(''.join(output))
+
+
+input = Input()
+
+# PART ONE
+# print(sum([signal_strength_during(y, input.bonus) for y in range(20, 260, 40)]))
+# print(sum([signal_strength_during(y, input.bonus) for y in range(20, 260, 40)]))
+print(sum([signal_strength_during(y, input.puzzle) for y in range(20, 260, 40)]))
+
+# PART TWO
+# print(draw(input.bonus))
+print(draw(input.puzzle))

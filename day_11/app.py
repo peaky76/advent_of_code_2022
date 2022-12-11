a81239889127bbfd6d6c12a0c00c27a6f38d101e
Robert Peacock <peaky76>
2022-12-11 12:12:35 +0000

    Day 11 complete

---
+++ day_11/app.py
@@ -0,0 +1,77 @@
+import operator
+import sys
+
+sys.path.append("..")
+from input import Input
+from math import lcm
+
+class Monkey:
+
+    OPS = {
+        '+' : operator.add,
+        '-' : operator.sub,
+        '*' : operator.mul,
+        '//' : operator.floordiv
+    }
+
+    def __init__(self, num, operator, adjustor, test_divisible_by_n, true_to, false_to, objects):
+        self.num = num
+        operator = Monkey.OPS[operator]
+        self.operation = lambda x: operator(x, x) if adjustor == 'old' else operator(x, int(adjustor))
+        self.test_divisible_by_n = test_divisible_by_n
+        self.true_to = true_to
+        self.false_to = false_to
+        self.objects = objects
+        self.inspection_count = 0
+        self.worry_reducer = lambda x: x // 3
+
+    def distribute(self):
+        self.inspect()
+        distribution_list = [(self.true_to, object) if self.test(object) else (self.false_to, object) for object in self.objects]
+        self.objects = []
+        return distribution_list
+
+    def inspect(self):
+        self.objects = [self.worry_reducer(self.operation(object)) for object in self.objects]
+        self.inspection_count += len(self.objects)
+
+    def test(self, x):
+        return x % self.test_divisible_by_n == 0
+
+def parse_monkeys(source):
+    monkeys = ''.join(line for line in source if line).split('Monkey ')[1:]
+    for monkey in monkeys:
+        num, rem = monkey.split(':  Starting items: ')
+        object_str, rem = rem.split('  Operation: new = old ')
+        op_str, rem = rem.split('  Test: divisible by ')
+        operator, adjustor = op_str.split(' ')
+        test_divisible_by_n, rem = rem.split('    If true: throw to monkey ')
+        true_to, false_to = rem.split('    If false: throw to monkey ')
+        objects = [int(x) for x in object_str.split(',')]
+        yield Monkey(int(num), operator, adjustor, int(test_divisible_by_n), int(true_to), int(false_to), objects)
+
+def play_piggy_in_the_middle(monkeys, rounds):
+    for _ in range(rounds):
+        for monkey in monkeys:
+            for item in monkey.distribute():
+                monkeys[item[0]].objects.append(item[1])
+    return monkeys
+
+def calc_monkey_business(source, rounds, worry_more = False):
+    monkeys = [m for m in parse_monkeys(source)]
+    m_lcm = lcm(*[m.test_divisible_by_n for m in monkeys])
+    if worry_more:
+        for monkey in monkeys:
+            monkey.worry_reducer = lambda x: x % m_lcm if x > m_lcm else x
+    counts = sorted([monkey.inspection_count for monkey in play_piggy_in_the_middle(monkeys, rounds)])
+    return counts[-2] * counts[-1]
+    
+input = Input()
+
+# PART ONE
+# print(calc_monkey_business(input.example, 20))
+print(calc_monkey_business(input.puzzle, 20))
+
+# PART TWO
+# print(calc_monkey_business(input.example, 10000, True))
+print(calc_monkey_business(input.puzzle, 10000, True))
